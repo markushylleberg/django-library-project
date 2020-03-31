9448382d0bdb3932bf816d3182f30bf629947e33
Markus Hylleberg <markushylleberg@gmail.com>
2020-03-31 10:35:56 +0200

    tidying things up and finishing project

--- loan/views.py
+++ loan/views.py
@@ -21,29 +21,37 @@ def new_loan(req):
         current_magazine_loans_amount = Loan.objects.filter(user=req.user, is_returned=False, book_id__material_id__material_type='magazine').count()
 
         if current_book_loans_amount == 10 and current_magazine_loans_amount == 3:
-            pass
+            print('The user has reached the maximum loan amount of books and magazines.')
         else:
             if book_data[0].material_type == 'magazine' and current_magazine_loans_amount == 3 or book_data[0].material_type == 'book' and current_book_loans_amount == 10:
-                print('Do nothing.')
+                print('The user has reached the maximum of either books or magazines.')
             else:
-                ### Define the return date
+                ### Check if user has expired material - if they have -> deny loan
                 current_date = datetime.now()
 
-                if book_data[0].material_type == 'magazine':
-                    return_date = current_date+timedelta(days=7)
+                does_user_have_expired_material = Loan.objects.filter(user=user_id, loan_return_datetime__lt=current_date, is_returned=False).exists()
+                if does_user_have_expired_material:
+                    print('User has expired material! The user cannot loan more before returnin the expired material.')
                 else:
-                    return_date = current_date+timedelta(days=30)
+                    ### APPROVED ( NO MAXIMUM AMOUNT OF LOAN ALREADY || NO EXPIRED MATERIAL ) :
+                    ### Define the return date
+                    current_date = datetime.now()
 
-                new_loan = Loan()
-                new_loan.user = user_id
-                new_loan.book_id = book_id[0]
-                new_loan.loan_return_datetime = return_date
-                new_loan.save()
+                    if book_data[0].material_type == 'magazine':
+                        return_date = current_date+timedelta(days=7)
+                    else:
+                        return_date = current_date+timedelta(days=30)
 
-                ### Change status to is_loaned = True on given published material
-                book_loaned = Publishmaterial.objects.get(id=book_post)
-                book_loaned.is_loaned = True
-                book_loaned.save()
+                    new_loan = Loan()
+                    new_loan.user = user_id
+                    new_loan.book_id = book_id[0]
+                    new_loan.loan_return_datetime = return_date
+                    new_loan.save()
+
+                    ### Change status to is_loaned = True on given published material
+                    book_loaned = Publishmaterial.objects.get(id=book_post)
+                    book_loaned.is_loaned = True
+                    book_loaned.save()
 
         return HttpResponseRedirect(reverse('dashboard:dashboard'))
 
